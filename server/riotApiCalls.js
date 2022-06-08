05d373184f40e87db3354ddb9ec46797a25541ae
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2022-06-07 23:09:28 -0700

    able to get match history information from match id list

--- server/riotApiCalls.js
+++ server/riotApiCalls.js
@@ -14,21 +14,26 @@ export function getCurrentGame(summonerName){
 }
 
 export async function getPlayerHistory(summonerName){
-    //from summoner name get their puuid
+    //get summoner puuid from name
     const res = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiCallKey}`);
-    const json = res.json();
+    const json = await res.json();
     const puuid = json["puuid"];
 
-    // from puuid get match id list
+    //get match id list from puuid
     const matchFetch = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${apiCallKey}`);
     const matchIds = await matchFetch.json();
 
-    // use match id list to get game data for each match
-    // const matchData = await matchIds.map(matchId => {
+    //get match data from each match id 
+    //note when '{}' is used here the return needs to be explicit
+    //the return is implicit if it is written like
+    // const responses = await Promise.all(Object.values(matchIds).map(matchId => 
     //     fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${apiCallKey}`)
-    // }) 
-    // const matches = await Promise.all(matchData).then(match => {
-    //     match.json();
-    // })
-    return matches;
+    // ));
+    const responses = await Promise.all(Object.values(matchIds).map(matchId => {
+        return fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${apiCallKey}`)
+    }));
+    const jsons = await Promise.all(responses.map(r => {
+        return r.json()
+    }));
+    return jsons
 }
