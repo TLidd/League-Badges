84c3c167e1eebb1cdc0508e008d6655a6070fa4b
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2022-07-18 01:35:50 -0700

    set up a grid for the badges and a title for the current badges to show on hover

--- client/src/components/SummonerCard.js
+++ client/src/components/SummonerCard.js
@@ -9,6 +9,12 @@ const SummonerCard = ({sumName, sumRole, sumBadges, activeGame}) => {
     summonerName = name;
   }
 
+  let badgeDescriptions = {
+    Warder: "The Summoner's Vision Score",
+    TowerDestroyer: "The Damage done to structures",
+    CreepKiller: "The Summoner's creep score",
+  }
+
   let nameMatch = name.toUpperCase() === summonerName.toUpperCase();
   let highlight = nameMatch ? "highlightSummoner" : "lobbyParticipant";
 
@@ -26,15 +32,17 @@ const SummonerCard = ({sumName, sumRole, sumBadges, activeGame}) => {
       <div>
         {sumRole && sumBadges && 
           <div>
-              <img src={require(`../../public/roleIcons/${sumRole}.png`)} alt="Summoner Role" className="roleIcon"/>
-              <div>
-                {
-                  Object.keys(sumBadges).map((badge) => {
-                    return <div className={`${sumBadges[badge]}`} key={`${badge}`}>
-                              {badge}
-                            </div>
-                  })
-                }
+              <img title="Most Played Role" src={require(`../../public/roleIcons/${sumRole}.png`)} alt="Summoner Role" className="roleIcon"/>
+              <div className="grid-container">
+                <div className="badges">
+                  {
+                      Object.keys(sumBadges).map((badge) => {
+                        return <div title={badgeDescriptions[badge]} className={`${sumBadges[badge]} badge`} key={`${badge}`}>
+                                    {badge}
+                                </div>
+                      })
+                  }
+                </div>
               </div>
           </div>
         }
